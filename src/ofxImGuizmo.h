7b0d3677c0c0c916fd2b4ab1de38668d006cba38
nariakiiwatani <nariakiiwatani@annolab.com>
2020-12-24 13:01:09 +0900

    add viewvolume arg mainly for orthographic camera

--- src/ofxImGuizmo.h
+++ src/ofxImGuizmo.h
@@ -8,16 +8,17 @@
 
 namespace ImGuizmo
 {
-static bool Manipulate(const ofCamera &camera, glm::mat4 &matrix, OPERATION operation, MODE mode, glm::mat4 *delta_matrix=nullptr, const float *snap = nullptr, const float *localBounds = nullptr, const float *boundsSnap = nullptr) {
+static bool Manipulate(const ofCamera &camera, glm::mat4 &matrix, OPERATION operation, MODE mode, const ofRectangle *viewvolume=nullptr, glm::mat4 *delta_matrix=nullptr, const float *snap = nullptr, const float *localBounds = nullptr, const float *boundsSnap = nullptr) {
 	ImGuiIO& io = ImGui::GetIO();
 	ImGuizmo::SetRect(0, 0, io.DisplaySize.x, io.DisplaySize.y);
 	auto view = glm::inverse(camera.getGlobalTransformMatrix());
-	auto proj = camera.getProjectionMatrix();
+	auto proj = viewvolume ? camera.getProjectionMatrix(*viewvolume) : camera.getProjectionMatrix();
+	ImGuizmo::SetOrthographic(camera.getOrtho());
 	return ImGuizmo::Manipulate(&view[0][0], &proj[0][0], operation, mode, &matrix[0][0], delta_matrix?&((*delta_matrix)[0][0]):nullptr, snap, localBounds, boundsSnap);
 }
-static bool Manipulate(const ofCamera &camera, ofNode &node, OPERATION operation, MODE mode, glm::mat4 *delta_matrix=nullptr, const float *snap = nullptr, const float *localBounds = nullptr, const float *boundsSnap = nullptr) {
+static bool Manipulate(const ofCamera &camera, ofNode &node, OPERATION operation, MODE mode, const ofRectangle *viewvolume=nullptr, glm::mat4 *delta_matrix=nullptr, const float *snap = nullptr, const float *localBounds = nullptr, const float *boundsSnap = nullptr) {
 	auto matrix = node.getGlobalTransformMatrix();
-	if(Manipulate(camera, matrix, operation, mode, delta_matrix, snap, localBounds, boundsSnap)) {
+	if(Manipulate(camera, matrix, operation, mode, viewvolume, delta_matrix, snap, localBounds, boundsSnap)) {
 		glm::vec3 scale;
 		glm::quat rotation;
 		glm::vec3 translation;
